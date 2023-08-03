c14f93d378d68ecc3213e0945b61bafa2e3eff6f
Steve J. South(NamJungGu) <nowage@gmail.com>
2023-08-04 00:52:01 +0900

    Delete Temporary File

--- .history/Ubuntu/20.04/install_basic_20230802144027.sh
+++
@@ -1,9 +0,0 @@
-#Base Package
-apt install -y curl tmux build-essential manpages-dev
-
-## Virtual Box
-
-## Vagrant
-
-## Vnc Server
-apt install -y tigervnc-standalone-server tigervnc-xorg-extension
